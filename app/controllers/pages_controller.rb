eaf5331f80a1c9730b4c6c31c6bd8b6dbb2160b4
dev global <andreeacristiana00@yahoo.com>
2023-08-07 12:55:25 +0300

    Post-Feedback Updates 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,51 +2,17 @@ class PagesController < ApplicationController
   def home
     @products = Product.all
     
-    if params[:category] == 'entree'
-      @products = @products.where(category: 0)
-    elsif params[:category] == 'second_course'
-      @products = @products.where(category: 1)
-    elsif params[:category] == "salad"
-      @products = @products.where(category: 2)
-    elsif params[:category] == "pizza"
-      @products = @products.where(category: 3)
-    elsif params[:category] == "dessert"
-      @products = @products.where(category: 4)
-    end
+    @products = @products.where(category: :entree) if params[:category] == "entree"
+    @products = @products.where(category: :second_course) if params[:category] == "second_course"
+    @products = @products.where(category: :salad) if params[:category] == "salad"
+    @products = @products.where(category: :pizza) if params[:category] == "pizza"
+    @products = @products.where(category: :dessert) if params[:category] == "dessert"
 
-    if params[:vegetarian] == 'non_vegetarian'
-      @products = @products.where(vegetarian: 0)
-    elsif params[:vegetarian] == 'vegetarian'
-      @products = @products.where(vegetarian: 1)
-    end
+    @products = @products.order(:price) if params[:order] == "asc"
+    @products = @products.order(price: :desc) if params[:order] == "desc"
 
-    if params[:order] == 'descending'
-      @products = @products.order(price: :desc)
-    else params[:order] == 'ascending'
-      @products = @products.order(:price)
-    end
-
-        # @products_entree_vegetarian = Product.where(category: 0, vegetarian: 1)
-    # @products_entree_non_vegetarian = Product.where(category: 0, vegetarian: 0)
-    # @products_second_course_vegetarian = Product.where(category: 1, vegetarian: 1)
-    # @products_second_course_non_vegetarian = Product.where(category: 1, vegetarian: 0)
-    # @products_salad_vegetarian = Product.where(category: 2, vegetarian: 1)
-    # @products_salad_non_vegetarian = Product.where(category: 2, vegetarian: 0)
-    # @products_pizza_vegetarian = Product.where(category: 3, vegetarian: 1)
-    # @products_pizza_non_vegetarian = Product.where(category: 3, vegetarian: 0)
-    # @products_dessert_vegetarian = Product.where(category: 4, vegetarian: 1)
-    # @products_dessert_non_vegetarian = Product.where(category: 4, vegetarian: 0)
-
-    # @products = @products_entree_vegetarian.or(@products_entree_non_vegetarian).or(@products_second_course_vegetarian).or(@products_second_course_non_vegetarian).or(@products_salad_vegetarian).or(@products_salad_non_vegetarian).or(@products_pizza_non_vegetarian).or(@products_pizza_vegetarian).or(@products_dessert_non_vegetarian).or(@products_dessert_vegetarian)
-    # @products = Product.all
-    # @products = Product.where(category: 0) if params[:category] == 'entree'
-    # @products = Product.where(category: 1) if params[:category] == 'second_course'      
-    # @products = Product.where(category: 2)  if params[:category] == 'salad'      
-    # @products = Product.where(category: 3) if params[:category] == 'pizza'      
-    # @products = Product.where(category: 4) if params[:category] == 'dessert'
-    # @products = Product.order(:price)    
-    # @products = Product.where(vegetarian: 0) if params[:vegetarian] == 'non_vegetarian'
-    # @products = Product.where(vegetarian: 1) if params[:vegetarian] == 'vegetarian'
+    @products = @products.where(vegetarian: :non_vegetarian) if params[:vegetarian] == "non_vegetarian"
+    @products = @products.where(vegetarian: :vegetarian) if params[:vegetarian] == "vegetarian"
   end
 
   def help
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,25 @@
 Product.destroy_all
 
 product1 = Product.create(title: 'Pasta Carbonara', description: 'Second Courses', price: 10.99, category: 1, vegetarian: 0)
-product1.image.attach(io: File.open(Rails.root.join('app/assets/images/pasta_carbonara.png')), filename: 'pasta_carbonara.png')
+product1.image.attach(io: File.open(Rails.root.join('seed-images/pasta_carbonara.png')), filename: 'pasta_carbonara.png')
 
 product2 = Product.create(title: 'Chicken Soup', description: 'Entrees', price: 15.99, category: 0, vegetarian: 0)
-product2.image.attach(io: File.open(Rails.root.join('app/assets/images/soup_chicken.png')), filename: 'soup_chicken.png')
+product2.image.attach(io: File.open(Rails.root.join('seed-images/soup_chicken.png')), filename: 'soup_chicken.png')
 
 product3 = Product.create(title: 'Vitamin Salad', description: 'Salads', price: 20.49, category: 2, vegetarian: 1)
-product3.image.attach(io: File.open(Rails.root.join('app/assets/images/salad_vitamin.png')), filename: 'salad_vitamin.png')
+product3.image.attach(io: File.open(Rails.root.join('seed-images/salad_vitamin.png')), filename: 'salad_vitamin.png')
 
 product4 = Product.create(title: 'Pasta with Walnut', description: 'Second Courses', price: 20.99, category: 1, vegetarian: 1)
-product4.image.attach(io: File.open(Rails.root.join('app/assets/images/pasta_walnut.png')), filename: 'pasta_walnut.png')
+product4.image.attach(io: File.open(Rails.root.join('seed-images/pasta_walnut.png')), filename: 'pasta_walnut.png')
 
 product5 = Product.create(title: 'Pizza Capriciosa', description: 'Second Courses', price: 35.99, category: 3, vegetarian: 0)
-product5.image.attach(io: File.open(Rails.root.join('app/assets/images/pizza_capriciosa.png')), filename: 'pizza_capriciosa.png')
+product5.image.attach(io: File.open(Rails.root.join('seed-images/pizza_capriciosa.png')), filename: 'pizza_capriciosa.png')
 
 product6 = Product.create(title: 'Pancake with Fruit', description: 'Desserts', price: 14.49, category: 4, vegetarian: 0)
-product6.image.attach(io: File.open(Rails.root.join('app/assets/images/pancake.png')), filename: 'pancake.png')
+product6.image.attach(io: File.open(Rails.root.join('seed-images/pancake.png')), filename: 'pancake.png')
 
 product7 = Product.create(title: 'Caesar Salad', description: 'Salads', price: 22.99, category: 2, vegetarian: 0)
-product7.image.attach(io: File.open(Rails.root.join('app/assets/images/salad_caesar.png')), filename: 'salad_caesar.png')
+product7.image.attach(io: File.open(Rails.root.join('seed-images/salad_caesar.png')), filename: 'salad_caesar.png')
 
 product8 = Product.create(title: 'Green Salad', description: 'Salads', price: 19.49, category: 2, vegetarian: 1)
-product8.image.attach(io: File.open(Rails.root.join('app/assets/images/salad_green.png')), filename: 'salad_green.png')
+product8.image.attach(io: File.open(Rails.root.join('seed-images/salad_green.png')), filename: 'salad_green.png')
